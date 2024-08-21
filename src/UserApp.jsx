3f9e3ab906f2a5750579478799e238b0a9173061
Daniel Eduardo <118782478+DanielEduardoXx@users.noreply.github.com>
2024-08-20 22:15:25 -0500

    Boton Carrito
    Se Agrega boton para que el cliente visualice las compras que estan en preparacion

--- src/UserApp.jsx
+++ src/UserApp.jsx
@@ -1,44 +1,37 @@
-<<<<<<< Updated upstream
-// UserApp.jsx
-import React from 'react';
-=======
 
 import React, { useContext, useEffect, useState } from 'react';
->>>>>>> Stashed changes
 import { Route, Routes } from 'react-router-dom';
 import Inicio from './components/users/Inicio';
 import MenuLink from './components/users/MenuLink';
 import Productos from './components/users/Productos';
 import CardLogin from './components/users/common/CardLogin';
 import CardRegistro from './components/users/common/CardRegistro';
-import Cardpqr from './components/users/common/cardpqr.jsx'
 import CardPerfil from './components/users/common/CardPerfil';
 import ContinuaCompra from './components/users/common/ContinuaCompra';
 import CardCheckout from './components/users/common/CardCheckout';
 import Footer from './components/users/Footer.jsx';
-<<<<<<< Updated upstream
-=======
 import BtnCarrito from './components/users/common/BtnInfoCarrito.jsx';
 import { MyContext } from './services/MyContext.jsx';
->>>>>>> Stashed changes
 
 const UserApp = () => {
-  return (
-    <>
-      <MenuLink />
-      <Routes>
-        <Route path="inicio" element={<Inicio />} />
-        <Route path="contacto" element={<Cardpqr/>}/>
-        <Route path="productos" element={<Productos />} />
-        <Route path="iniciar-sesion" element={<CardLogin />} />
-        <Route path="registro" element={<CardRegistro />} />
-        <Route path="perfil" element={<CardPerfil />} />
-        <Route path="continua compra" element={<ContinuaCompra />} />
-        <Route path="checkout" element={<CardCheckout />} />
-      </Routes>
-      <Footer />
-    </>
-  );
+    const { user } = useContext(MyContext);
+
+    return (
+        <>
+            <MenuLink />
+            <BtnCarrito />
+            <Routes>
+                <Route path="inicio" element={<Inicio />} />
+                <Route path="productos" element={<Productos />} />
+                <Route path="iniciar-sesion" element={<CardLogin />} />
+                <Route path="registro" element={<CardRegistro />} />
+                <Route path="perfil" element={<CardPerfil />} />
+                <Route path="continua compra" element={<ContinuaCompra />} />
+                <Route path="checkout" element={<CardCheckout />} />
+            </Routes>
+            <Footer />
+        </>
+    );
 };
 
 export default UserApp;
