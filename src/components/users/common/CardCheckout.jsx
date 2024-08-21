c513aaff7385f16b6833c47e8dc7028c1a08a532
Daniel Eduardo <118782478+DanielEduardoXx@users.noreply.github.com>
2024-08-20 21:03:36 -0500

    Actualizacion
    Se corrigen errores al enviar los datos a la tabla de ventas y detalle venta

--- src/components/users/common/CardCheckout.jsx
+++ src/components/users/common/CardCheckout.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Box, Paper, TextField, Button, MenuItem, FormControl, Select, InputLabel, Typography, Modal, Fade } from "@mui/material";
 import { styled } from '@mui/material/styles';
-import { useNavigate } from 'react-router-dom';  // Importa useNavigate
-import CardResumenCarrito from './CardResumenCarrito';
+import { useNavigate } from 'react-router-dom';
 import { MyContext } from "../../../services/MyContext";
-import { ventasUser } from '../../../services/ventasUser';
-import { detVentasUser } from '../../../services/ventasUser';  // Asegúrate de importar esto
+import { ventasUser, detVentasUser } from '../../../services/ventasUser';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
 const StyledForm = styled('form')(({ theme }) => ({
     '& > *': {
@@ -28,16 +28,22 @@ const ModalContent = styled(Box)(({ theme }) => ({
 function CardCheckout() {
     const [formData, setFormData] = useState({
         metodo_pago: '',
+        address_ventas: '',
+        tipo_de_entrega: '',
         estado: 1,
         total: 0,
         user_id: null
     });
     const [compra, setCompra] = useState([]);
     const [total, setTotal] = useState(0);
-    const [open, setOpen] = useState(false);  // Estado para controlar el modal
+    const [open, setOpen] = useState(false);
+    const [ventaId, setVentaId] = useState(null);
+    const [address, setAddress] = useState(null);
+    const [error, setError] = useState(null);
+    const [isDireccionEnabled, setIsDireccionEnabled] = useState(false);
 
     const { user } = useContext(MyContext);
-    const navigate = useNavigate();  // Hook para redirección
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (user) {
@@ -47,41 +53,84 @@ function CardCheckout() {
             const newTotal = storedCart.reduce((acc, item) => acc + item.producto.precio_producto * item.cantidad, 0);
             setTotal(newTotal);
 
-            setFormData((prevData) => ({
+            setFormData(prevData => ({
                 ...prevData,
                 total: newTotal,
-                user_id: user.user.id
+                user_id: user.user.id,
+                address_ventas: address
             }));
         }
     }, [user]);
 
     useEffect(() => {
-        if (user) {
-            if (compra.length > 0) {
-                localStorage.setItem(`cart_${user.user.id}`, JSON.stringify(compra));
-            }
+        if (user && user.user.direccion) {
+            setAddress(user.user.direccion);
+        }
+    }, [user]);
+
+    useEffect(() => {
+        if (user && compra.length > 0) {
+            localStorage.setItem(`cart_${user.user.id}`, JSON.stringify(compra));
         }
     }, [compra, user]);
 
     const handleChangeMetodoPago = (event) => {
-        setFormData((prevData) => ({
+        setFormData(prevData => ({ ...prevData, metodo_pago: event.target.value }));
+    };
+
+    const handleChangeTipoDeEntrega = (event) => {
+        const value = event.target.value;
+        setFormData(prevData => ({ ...prevData, tipo_de_entrega: value }));
+        setIsDireccionEnabled(value === 'entrega_en_domicilio');
+    };
+
+    const handleChangeDireccion = (event) => {
+        const { value } = event.target;
+    
+        setFormData(prevData => ({
             ...prevData,
-            metodo_pago: event.target.value,
+            address_ventas: formData.tipo_de_entrega === 'recoger_en_sucursal' ? null : value
         }));
     };
 
     const handleClose = () => {
         setOpen(false);
-        navigate('/inicio');  // Redirige a la ruta deseada
+        navigate('/inicio');
+        window.location.reload();
     };
+
+    useEffect(() => {
+        if (error) {
+            const timer = setTimeout(() => {
+                setError('');
+            }, 3000);
+            return () => clearTimeout(timer);
+        }
+    }, [error]);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        if (!formData.metodo_pago) {
-            console.error('El campo metodo_pago es obligatorio.');
+        // Validación de campos
+        if (!formData.metodo_pago || !formData.tipo_de_entrega) {
+            setError('El metodo de Pago y Tipo de Entrega Son Obligatorios')
+            console.error('El campo metodo_pago y tipo_de_entrega son obligatorios.');
             return;
         }
+
+        if (formData.tipo_de_entrega === 'entrega_en_domicilio' && !formData.address_ventas) {
+            setError('La dirección de entrega es obligatoria')
+
+            console.error('La dirección es obligatoria para la entrega en domicilio.');
+            return;
+        }
+
+        if (formData.tipo_de_entrega === 'recoger_en_sucursal') {
+            formData.address_ventas = null; // Asignar null si es recoger en sucursal
+        }
+
+        // Aquí puedes manejar el envío de los datos
+        console.log('Datos del formulario:', formData);
 
         if (!formData.total || !formData.user_id) {
             console.error('Todos los campos son obligatorios.');
@@ -89,16 +138,21 @@ function CardCheckout() {
         }
 
         try {
-            const ventaData = {
-                ...formData,
-                total: total,
-                user_id: user.user.id
-            };
-
+            // Paso 1: Enviar datos de la venta
+            const ventaData = { ...formData, user_id: user.user.id };
             const ventaResponse = await ventasUser(ventaData);
 
             if (ventaResponse && ventaResponse.data) {
                 const ventaId = ventaResponse.data.id;
+                setVentaId(ventaId);
+
+                // Guardar ID de venta en localStorage
+                const key = `venta_${user.user.id}`;
+                const storedVentas = JSON.parse(localStorage.getItem(key)) || [];
+                const updatedVentas = [...storedVentas, ventaId];
+                localStorage.setItem(key, JSON.stringify(updatedVentas));
+
+                // Paso 2: Enviar detalles de la venta
                 const detVentaData = {
                     detalles: compra.map(item => ({
                         nom_producto: item.producto.nom_producto,
@@ -106,18 +160,26 @@ function CardCheckout() {
                         cantidad: item.cantidad,
                         subtotal: item.producto.precio_producto * item.cantidad,
                         venta_id: ventaId
-                    }))
+                    })),
+                    total: total
                 };
+                await detVentasUser(detVentaData);
 
-                const detResponse = await detVentasUser(detVentaData);
+                // Actualizar carrito y almacenar compras
+                const cartKey = `cart_${user.user.id}`;
+                const comprasKey = `compras_${user.user.id}`;
+                const storedCart = JSON.parse(localStorage.getItem(cartKey)) || [];
+                const storedCompras = JSON.parse(localStorage.getItem(comprasKey)) || [];
+                const updatedCompras = [...storedCompras, ...storedCart];
+                localStorage.setItem(comprasKey, JSON.stringify(updatedCompras));
 
-                if (detResponse && detResponse.message === 'Registros creados exitosamente') {
-                    localStorage.removeItem(`cart_${user.user.id}`);
-                    localStorage.removeItem(`cantidades_${user.user.id}`);
-                    setCompra([]);
-                    setTotal(0);
-                    setOpen(true);  // Abrir el modal
-                }
+                // Eliminar el carrito actual
+                localStorage.removeItem(cartKey);
+                localStorage.removeItem(`cantidades_${user.user.id}`);
+
+                setCompra([]);
+                setTotal(0);
+                setOpen(true);  // Abrir el modal
             }
         } catch (error) {
             console.error('Error al enviar la venta:', error.message);
@@ -129,89 +191,123 @@ function CardCheckout() {
             <Paper sx={{ padding: '2rem', width: '100%', maxWidth: '600px' }}>
                 <StyledForm onSubmit={handleSubmit}>
                     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                        <Box>
-                            <TextField
-                                disabled
-                                sx={{ margin: '5px 0', width: '100%' }}
-                                label="Nombres"
-                                variant="outlined"
-                                defaultValue={user ? user.user.name : ''}
-                            />
-                            <TextField
-                                disabled
-                                sx={{ margin: '5px 0', width: '100%' }}
-                                label="Telefono"
-                                type="number"
-                                variant="outlined"
-                                defaultValue={user ? user.user.tel : ''}
-                            />
-                            <TextField
-                                sx={{ margin: '5px 0', width: '100%' }}
-                                label="Direccion"
-                                type="text"
-                                variant="outlined"
-                                defaultValue={user ? user.user.direccion : ''}
-                            />
-                            <TextField
-                                disabled
-                                sx={{ margin: '5px 0', width: '100%' }}
-                                id="id"
-                                label="Identificacion"
-                                type="number"
-                                variant="outlined"
-                                defaultValue={user ? user.user.id : ''}
-                            />
-                            <FormControl fullWidth sx={{ margin: '5px 0' }}>
-                                <InputLabel id="metodo_pago">Metodo de Pago</InputLabel>
-                                <Select
-                                    labelId="metodo_pago"
-                                    id="metodo_pago"
-                                    value={formData.metodo_pago}
-                                    label="Metodo de Pago"
-                                    onChange={handleChangeMetodoPago}
-                                >
-                                    <MenuItem value={'efectivo'}>Efectivo</MenuItem>
-                                    <MenuItem value={'nequi'}>Nequi</MenuItem>
-                                    <MenuItem value={'daviplata'}>Daviplata</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Box>
-                        <Box>
-                            {compra.map((item) => (
-                                <CardResumenCarrito
-                                    key={item.id}
-                                    id={item.id}
-                                    titulo={item.producto.nom_producto}
-                                    precio={item.producto.precio_producto}
-                                    foto={item.producto.foto || '/path/to/default/image.jpg'}
-                                    noProductos={item.cantidad}
+                        <Box sx = {{ display: 'flex'}}>
+                            <Box>
+                                <TextField
+                                    disabled
+                                    sx={{ margin: '5px 0', width: '100%' }}
+                                    label="Nombres"
+                                    variant="outlined"
+                                    defaultValue={user ? user.user.name : ''}
                                 />
-                            ))}
+                                <TextField
+                                    disabled
+                                    sx={{ margin: '5px 0', width: '100%' }}
+                                    label="Telefono"
+                                    type="number"
+                                    variant="outlined"
+                                    defaultValue={user ? user.user.tel : ''}
+                                />
+                                <TextField
+                                    sx={{ margin: '5px 0', width: '100%' }}
+                                    label="Direccion"
+                                    type="text"
+                                    variant="outlined"
+                                    onChange={handleChangeDireccion}
+                                    disabled={!isDireccionEnabled}
+                                />
+                                <TextField
+                                    disabled
+                                    sx={{ margin: '5px 0', width: '100%' }}
+                                    id="id"
+                                    label="Identificacion"
+                                    type="number"
+                                    variant="outlined"
+                                    defaultValue={user ? user.user.id : ''}
+                                />
+                                <FormControl fullWidth sx={{ margin: '5px 0' }}>
+                                    <InputLabel>Metodo de Pago</InputLabel>
+                                    <Select
+                                        label="Metodo de Pago"
+                                        value={formData.metodo_pago}
+                                        onChange={handleChangeMetodoPago}
+                                    >
+                                        <MenuItem value="tarjeta">Tarjeta</MenuItem>
+                                        <MenuItem value="efectivo">Efectivo</MenuItem>
+                                    </Select>
+                                </FormControl>
+                                <FormControl fullWidth sx={{ margin: '5px 0' }}>
+                                    <InputLabel>Tipo de Entrega</InputLabel>
+                                    <Select
+                                        label="Tipo de Entrega"
+                                        value={formData.tipo_de_entrega}
+                                        onChange={handleChangeTipoDeEntrega}
+                                    >
+                                        <MenuItem value="recoger_en_sucursal">Recoger en Sucursal</MenuItem>
+                                        <MenuItem value="entrega_en_domicilio">Entrega en Domicilio</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Box>
+                            <Box sx = {{display:'flex', flexDirection:'column'}}>
+                                <Typography variant="h6" sx={{
+                                    fontWeight: 'bold', marginBottom: '0.5rem',
+                                    display: 'flex',
+                                    justifyContent: 'center'
+                                }}> Productos:</Typography>
+                                {compra.length > 0 ? (
+                                    <>
+                                        {compra.map((item) => (
+                                            <Paper key={item.id} sx={{ padding: '1rem', margin: '1rem 0' }}>
+                                                <Typography>
+                                                    {item.producto.nom_producto} - {item.cantidad} x ${item.producto.precio_producto}
+                                                </Typography>
+                                            </Paper>
+                                        ))}
+                                        <Typography variant="h6" sx={{ marginTop: '1rem' }}>
+                                            Valor Total: $ {total.toLocaleString('en-US')}
+                                        </Typography>
+                                    </>
+                                ) : (
+                                    <Typography variant="body1" align="center">
+                                        El carrito está vacío.
+                                    </Typography>
+                                )}
+                            </Box>
+
+
+
+               
                         </Box>
                     </Box>
-                    <Typography variant="h6" sx={{ marginTop: '1rem' }}>
-                        Valor Total: $ {total.toLocaleString('en-US')}
-                    </Typography>
-                    <Button type="submit" variant="contained" color="primary" sx={{ width: '100%', marginTop: '1rem' }}>
-                        Terminar Compra
-                    </Button>
+                    <Button type="submit" variant="contained" color="primary" sx={{ margin: '10px 0' }}>
+                                Finalizar Compra
+                            </Button>
                 </StyledForm>
-            </Paper>
 
+                {error && (
+
+                    <Stack sx={{ width: '100%' }} spacing={2}>
+                        <Alert severity="error">{error}</Alert>
+                    </Stack>
+
+                )}
+            </Paper>
             <Modal
-                style={{ zIndex: "20" }} 
-                open={open} 
-                onClose={handleClose} 
-                aria-labelledby="modal-modal-title" 
-                aria-describedby="modal-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropProps={{ timeout: 500 }}
             >
                 <Fade in={open}>
                     <ModalContent>
-                        <Typography variant="h6" align="center">
-                            ¡Compra realizada correctamente!
+                        <Typography variant="h6" gutterBottom>
+                            ¡Compra realizada con éxito!
                         </Typography>
-                        <Button onClick={handleClose} variant="contained" color="primary" fullWidth sx={{ marginTop: '1rem' }}>
-                            Cerrar
+                        <Typography variant="body1">
+                            Tu ID de venta es: {ventaId}
+                        </Typography>
+                        <Button variant="contained" color="primary" onClick={handleClose} sx={{ marginTop: '1rem' }}>
+                            Volver al Inicio
                         </Button>
                     </ModalContent>
                 </Fade>
